e58b6ba1a2f6690a03e5b1a0300a2be14da58878
Gaurav Kohli <gaurav.kohli@pinelabs.com>
2021-09-05 15:36:43 +0530

    config changes

--- src/Constants/Config.js
+++ src/Constants/Config.js
@@ -1,6 +1,6 @@
 module.exports =Object.freeze({
-    BACKEND_URL:/*"https://gaureeshabackend.herokuapp.com/",//*/"http://localhost:4000/",
-    FRONTEND_URL:/*"https://gaureesha-com.herokuapp.com/",//*/"http://localhost:3000/",
+    BACKEND_URL:"https://gaureeshabackend.herokuapp.com/",//"http://localhost:4000/",
+    FRONTEND_URL:"https://gaureesha-com.herokuapp.com/",//"http://localhost:3000/",
     NOTIFICATION_CLOSE_TIME:4000,
     NOTIFICATION_THEME:"colored"
 });
