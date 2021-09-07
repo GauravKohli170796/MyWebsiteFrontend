52175b09c30efe7e961e572d962fceb6d9b61a09
Gaurav Kohli <gaurav.kohli@pinelabs.com>
2021-09-07 19:44:24 +0530

    changes for logo touch

---
+++ src/Components/AboutGaureesha.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function AboutGaureesha() {
+    return (
+        <div className="AboutGaureeshaCls">
+            <div className="nameEffect">
+                <div className="Fistname">
+                    <span>Gaurav</span>
+
+                </div>
+                <div classname="Secondname">
+                <span>Deeksha</span>
+                </div>
+                <div className="pluscomming">
+                    <span>+</span>
+
+                </div>
+                <div>
+                <span><span>❤</span> Gaureesha.com <span>❤</span></span>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default AboutGaureesha;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 import "../styles/Navbar.css";
+import { useHistory } from "react-router-dom";
 
 function Header(props) {
+    const history=useHistory();
     return (
         <header>
         <div className="headIcon">
             <button className="btn" onClick={() => {
                 props.setbNavbarShow(!props.bNavbarShow);
             }}>{props.bNavbarShow ===true ? <FaTimes></FaTimes> : <FaBars></FaBars>}</button>
-            <span className="webTitle"><span className="red">❤</span> Gaureesha.com <span className="red">❤</span></span>
+            <span className="webTitle"><span className="red">❤</span> <button className="mainHeaderstyle" onClick={()=>{history.push('/Home')}}>Gaureesha.com</button><span className="red">❤</span></span>
 
         </div>
     </header>
